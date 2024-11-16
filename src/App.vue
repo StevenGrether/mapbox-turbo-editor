76c481aeac49965f4c83f32338fb32d98cd8d938
Steven Grether <steven.grether@inconnect.de>
2024-11-16 19:17:44 +0100

    add map

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-    <div></div>
+    <div ref="mapContainer" class="h-full w-full"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts">
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoidW5leHBlY3RlZGVycm9yIiwiYSI6ImNsODE4OXlyMzBkbGwzb253amJwaWh0OXUifQ.2Jeic3e1-h5EWG6qm9XAJw';
+
+export default {
+    beforeMount() {
+        const mapContainer = this.$refs.mapContainer;
+        if (!mapContainer) return;
+
+        const map = new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/outdoors-v12',
+            projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
+            zoom: 1,
+            center: [30, 15],
+        });
+    },
+};
+</script>
