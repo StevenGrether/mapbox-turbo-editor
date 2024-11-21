d462a531f91de085e6a2af1d26517046a24ec4c2
Steven Grether <steven.grether@inconnect.de>
2024-11-21 16:10:10 +0100

    implement direct select mode

--- src/TurboDirectSelectMode.ts
+++ src/TurboDirectSelectMode.ts
@@ -1,6 +1,60 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { createPreviewPoint, getSnappedPoint, getVertexCollection } from './utils';
+import { FeatureCollection, Point } from 'geojson';
+import { point } from '@turf/turf';
+import { LngLat } from 'mapbox-gl';
 
 const DirectSelect = MapboxDraw.modes.direct_select;
 const TurboDirectSelect = { ...DirectSelect };
 
-export default TurboDirectSelect
+TurboDirectSelect.onSetup = function (opts) {
+    const snapThreshold = opts.snapThreshold || 50;
+    const snapLngLat = null;
+
+    const state = DirectSelect.onSetup?.call(this, opts);
+    const vertexCollection = getVertexCollection(this);
+
+    const previewPoint = createPreviewPoint(this);
+
+    return { vertexCollection, snapThreshold, snapLngLat, previewPoint, ...state };
+};
+
+TurboDirectSelect.onDrag = function (state, e) {
+    DirectSelect.onDrag?.call(this, state, e);
+    if (state.selectedCoordPaths.length > 1) return;
+
+    const vertexCollection: FeatureCollection<Point> = state.vertexCollection;
+    if (!vertexCollection.features.length) {
+        if (this.getFeature('preview-point')) this.deleteFeature('preview-point');
+        state.snapLngLat = e.lngLat;
+        return;
+    }
+    const { lng, lat } = e.lngLat;
+    const mousePoint = point([lng, lat]);
+    const nearestVertex = getSnappedPoint(mousePoint, vertexCollection, state.snapThreshold);
+    if (!nearestVertex) {
+        if (this.getFeature('preview-point')) this.deleteFeature('preview-point');
+        state.snapLngLat = e.lngLat;
+        return;
+    }
+    const nearestCoordinates = nearestVertex.geometry.coordinates;
+    state.snapLngLat = new LngLat(nearestCoordinates[0], nearestCoordinates[1]);
+
+    state.previewPoint.setCoordinates([state.snapLngLat.lng, state.snapLngLat.lat]);
+    if (!this.getFeature('preview-point')) {
+        this.addFeature(state.previewPoint);
+    }
+};
+
+TurboDirectSelect.onMouseUp = function (state, e) {
+    if (state.selectedCoordPaths.length !== 1) return;
+    console.log(state.feature.updateCoordinate(state.selectedCoordPaths[0], state.snapLngLat.lng, state.snapLngLat.lat));
+    DirectSelect.onMouseUp?.call(this, state, e);
+};
+
+TurboDirectSelect.onStop = function (state) {
+    if (this.getFeature('preview-point')) this.deleteFeature('preview-point');
+    DirectSelect.onStop?.call(this, state);
+};
+
+export default TurboDirectSelect;
